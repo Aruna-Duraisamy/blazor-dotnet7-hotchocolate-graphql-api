d8ba02d7166e1ce0f23f5240dd856a54139fb5d8
arunaganeshraj <aruna.durai@gmail.com>
2022-11-28 16:53:47 +0000

    Add Hotchocolate, configure to display hello world

--- graphql.api/Program.cs
+++ graphql.api/Program.cs
@@ -1,3 +1,6 @@
+using System.Collections.Immutable;
+using graphql.api.Schema;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddGraphQLServer().AddQueryType<Query>();
 
 var app = builder.Build();
 
@@ -21,5 +25,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGraphQL();
 
 app.Run();
